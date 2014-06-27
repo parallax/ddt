df62984940d14e9e42ac1a1cfe2b65e15cf18828
Nathaniel Higgins <nat@nath.is>
2014-06-27 14:01:56 +0100

    Fixes #18

--- bindings/knockout.js
+++ bindings/knockout.js
@@ -4,6 +4,14 @@ define(['ddt', 'knockout'], function(ddt, ko) {
 
     var DATA_KEY = 'ddtObject';
 
+    function DDTErrorNoDDTBinding() {
+        this.message = 'Must apply the ddt binding to use this binding.';
+        this.stack = (new Error().stack);
+    }
+
+    DDTErrorNoDDTBinding.prototype = new Error();
+    DDTErrorNoDDTBinding.prototype.constructor = DDTErrorNoDDTBinding;
+
     ko.bindingHandlers.ddt = {
         init: function(element, valueAccessor) {
             var table = new ddt.DragAndDropTable($(element));
@@ -28,4 +36,24 @@ define(['ddt', 'knockout'], function(ddt, ko) {
         }
     };
 
+    ['verticalOnly', 'boundToTBody'].forEach(function(option) {
+        var key = 'ddt' + option[0].toUpperCase() + option.substr(1);
+
+        ko.bindingHandlers[key] = {
+            update : function(element, valueAccessor) {
+                var value = ko.unwrap(valueAccessor());
+                var table = ko.utils.domData.get(element, DATA_KEY);
+
+                if (!table) {
+                    throw new DDTErrorNoDDTBinding();
+                }
+
+                table[option] = value;
+            }
+        };
+    })
+
+    return {
+        DDTErrorNoDDTBinding : DDTErrorNoDDTBinding
+    };
 });
